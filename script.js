991177b15c23e41ff34aec20a6bdeba997435f1b
Sidney Trzepacz <sidtrzisyourleader@gmail.com>
2021-09-17 15:43:37 -0400

    update

--- script.js
+++ script.js
@@ -16,29 +16,28 @@ function doc_keyDown(e) {
     if (e.key === "Shift") {
         // call your function to do the thing
         for(i=0;i<btns.length;i++) {
-            btns[i].innerHTML = btns[i].innerHTML.toUpperCase();
-            btns[i].addEventListener("click", function() { copy(this.innerHTML) });
+            btns[i].textContent = btns[i].textContent.toUpperCase();
+            btns[i].addEventListener("click", function() { copy(this.textContent) });
         }
-        document.getElementById("dontChange").innerHTML = "ß";
+        document.getElementById("dontChange").textContent = "ß";
     }
 }
 function doc_keyUp(e) {
     for(i=0;i<btns.length;i++) {
-        btns[i].innerHTML = btns[i].innerHTML.toLowerCase();
-        btns[i].addEventListener("click", function() { copy(this.innerHTML) });
+        btns[i].textContent = btns[i].textContent.toLowerCase();
+        btns[i].addEventListener("click", function() { copy(this.textContent) });
     }
-    document.getElementById("dontChange").innerHTML = "ß";
+    document.getElementById("dontChange").textContent = "ß";
 }
 // register the handler 
 document.addEventListener('keydown', doc_keyDown, false);
 document.addEventListener('keyup', doc_keyUp, false);
 var radio = document.getElementsByClassName("contact-radio");
 for(i=0;i<radio.length;i++) {
-    console.log(i);
     radio[i].addEventListener("click", function() { localStorage.setItem("pref", this.id) })
 }
 for(i=0;i<btns.length;i++) {
-    btns[i].addEventListener("click", function() { copy(this.innerHTML) });
+    btns[i].addEventListener("click", function() { copy(this.textContent) });
 }
 if(localStorage.getItem("pref") == "FR") {
     document.getElementById("FR").click();
